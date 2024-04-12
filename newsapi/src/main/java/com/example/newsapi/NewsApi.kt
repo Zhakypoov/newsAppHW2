c59778be953aa8395d124eece6af115673785ae3
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-12 21:01:50 +0500

    video-1 part 2 add modules and change newsApi

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,7 +1,13 @@
 package com.example.newsapi
 
 import androidx.annotation.IntRange
-import kotlinx.serialization.SerialName
+import com.example.newsapi.models.Article
+import com.example.newsapi.models.Language
+import com.example.newsapi.models.Response
+import com.example.newsapi.models.SortBy
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
@@ -9,26 +15,34 @@ import java.util.Date
 interface NewsApi {
 
     @GET("/everything")
-    fun everything(
+    suspend fun everything(
         @Query("q") query: String? = null,
         @Query("from") from: Date? = null,
         @Query("to") to: Date? = null,
-        @Query("languages") languages: String? = null,
+        @Query("languages") languages: List<Language>? = null,
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pageSize") @IntRange(from = 0, to = 100) pageSize: Int = 100,
         @Query("page") @IntRange(from = 1) page: Int = 1,
-    )
+    ):Response<Article>
 }
 
-enum class SortBy{
+fun NewsApi(
+    baseUrl: String,
+    okHttpClient: OkHttpClient? = null
+): NewsApi{
+    val retrofit = retrofit(baseUrl, okHttpClient)
 
-    @SerialName("relevancy")
-    RELEVANCY,
-
-    @SerialName("popularity")
-    POPULARITY,
-
-    @SerialName("publishedAt")
-    PUBLISHED_AT
-
+    return retrofit.create()
 }
+
+
+private fun retrofit(
+    baseUrl: String,
+    okHttpClient: OkHttpClient?,
+): Retrofit{
+    return Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .run { if(okHttpClient != null) client(okHttpClient) else this }
+        .build()
+}
+
