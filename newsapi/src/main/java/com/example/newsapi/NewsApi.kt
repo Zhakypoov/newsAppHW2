44b2025f536137fb0ddea793823c4aad0b052803
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-12 20:12:35 +0500

    video-1 part 1 add newsApi

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -1,4 +1,34 @@
 package com.example.newsapi
 
-class NewsApi {
+import androidx.annotation.IntRange
+import kotlinx.serialization.SerialName
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Date
+
+interface NewsApi {
+
+    @GET("/everything")
+    fun everything(
+        @Query("q") query: String? = null,
+        @Query("from") from: Date? = null,
+        @Query("to") to: Date? = null,
+        @Query("languages") languages: String? = null,
+        @Query("sortBy") sortBy: SortBy? = null,
+        @Query("pageSize") @IntRange(from = 0, to = 100) pageSize: Int = 100,
+        @Query("page") @IntRange(from = 1) page: Int = 1,
+    )
+}
+
+enum class SortBy{
+
+    @SerialName("relevancy")
+    RELEVANCY,
+
+    @SerialName("popularity")
+    POPULARITY,
+
+    @SerialName("publishedAt")
+    PUBLISHED_AT
+
 }
