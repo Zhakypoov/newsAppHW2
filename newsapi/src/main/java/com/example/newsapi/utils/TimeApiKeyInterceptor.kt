da5c84fadbeb51d457ac265d1d07fdea598db736
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-12 21:52:19 +0500

    video-1 finish newsapi module

--- newsapi/src/main/java/com/example/newsapi/utils/TimeApiKeyInterceptor.kt
+++ newsapi/src/main/java/com/example/newsapi/utils/TimeApiKeyInterceptor.kt
@@ -5,10 +5,11 @@ import okhttp3.Response
 
 internal class TimeApiKeyInterceptor(private val apiKey: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        return chain.proceed(
-            chain.request().newBuilder()
+
+        val request = chain.request().newBuilder()
                 .addHeader("X-Api-Key", apiKey)
                 .build()
-        )
+        return chain.proceed(request)
+
     }
 }
