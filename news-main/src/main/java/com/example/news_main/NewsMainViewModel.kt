8a092bb5d0e531e436273eb298d23b229480b88a
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-13 02:35:54 +0500

    video-2 add requestResult viewmodel

--- news-main/src/main/java/com/example/news_main/NewsMainViewModel.kt
+++ news-main/src/main/java/com/example/news_main/NewsMainViewModel.kt
@@ -1,14 +1,26 @@
 package com.example.news_main
 
 import androidx.lifecycle.ViewModel
+import com.example.news.data.ArticleRepository
+import com.example.news.data.RequestResult
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-internal class NewsMainViewModel : ViewModel(){
+internal class NewsMainViewModel(
+  private val repository : ArticleRepository
+) : ViewModel(){
   private val _state = MutableStateFlow(State.None)
   val state: StateFlow<State>
     get() = _state.asStateFlow()
+
+  private fun RequestResult<List<Article>>.toState(): State{
+    return when(this){
+      is RequestResult.Error -> State.Error(data)
+      is RequestResult.InProgress -> State.Loading(data)
+      is RequestResult.Success -> State.Success(data)
+    }
+  }
 }
 
 sealed class State {
