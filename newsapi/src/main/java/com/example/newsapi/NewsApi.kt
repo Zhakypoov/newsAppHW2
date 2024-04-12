daa3a0255860f0c5148f6fff0557101b96ae9b30
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-12 21:22:11 +0500

    video-1 part 3 data serialization

--- newsapi/src/main/java/com/example/newsapi/NewsApi.kt
+++ newsapi/src/main/java/com/example/newsapi/NewsApi.kt
@@ -5,6 +5,10 @@ import com.example.newsapi.models.Article
 import com.example.newsapi.models.Language
 import com.example.newsapi.models.Response
 import com.example.newsapi.models.SortBy
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.create
@@ -28,20 +32,22 @@ interface NewsApi {
 
 fun NewsApi(
     baseUrl: String,
-    okHttpClient: OkHttpClient? = null
+    okHttpClient: OkHttpClient? = null,
+    json: Json = Json,
 ): NewsApi{
-    val retrofit = retrofit(baseUrl, okHttpClient)
-
-    return retrofit.create()
+    return retrofit(baseUrl, okHttpClient, json).create()
 }
 
 
 private fun retrofit(
     baseUrl: String,
     okHttpClient: OkHttpClient?,
+    json: Json,
 ): Retrofit{
+    val jsonConverterFactory = Json.asConverterFactory("application/json".toMediaType())
     return Retrofit.Builder()
         .baseUrl(baseUrl)
+        .addConverterFactory(jsonConverterFactory)
         .run { if(okHttpClient != null) client(okHttpClient) else this }
         .build()
 }
