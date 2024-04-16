caf7e023feed5f9565d176c8bd075f2a8f894921
Alzhan Zhakypov <120952004+Zhakypoov@users.noreply.github.com>
2024-04-16 14:36:07 +0500

    video-2 full

--- app/src/main/java/com/example/newsapp2/AppModule.kt
+++ app/src/main/java/com/example/newsapp2/AppModule.kt
@@ -2,14 +2,14 @@ package com.example.newsapp2
 
 import android.content.Context
 import com.example.database.NewsDatabase
-import com.example.news.data.ArticleRepository
+
 import com.example.newsapi.NewsApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
+
 import javax.inject.Singleton
 
 
@@ -18,6 +18,7 @@ import javax.inject.Singleton
 object AppModule {
 
     @Provides
+    @Singleton
     fun providerNewsApi(): NewsApi{
        return NewsApi(
            baseUrl = BuildConfig.NEWS_API_BASE_URL,
@@ -26,12 +27,8 @@ object AppModule {
     }
 
     @Provides
+    @Singleton
     fun providerNewsDatabase(@ApplicationContext context: Context): NewsDatabase{
         return NewsDatabase(context)
-    }
-
-    @Provides
-    fun providerArticlesRepository(): ArticleRepository{
-        return ArticleRepository()
     }
 }
